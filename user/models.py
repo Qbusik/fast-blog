a4f0b53c26a30282436f9516cef297d6a4baaadd
Jakub Paluch <qbusik@gmail.com>
2026-05-29 10:31:58 +0200

    Refactor user models

--- user/models.py
+++ user/models.py
@@ -1,21 +1,23 @@
-from pydantic import BaseModel, EmailStr
-from typing import Optional
+from pydantic import BaseModel, EmailStr, Field
 
 
-class User(BaseModel):
+class UserBase(BaseModel):
     username: str
-    email: Optional[str] = None
-    full_name: Optional[str] = None
-    disabled: Optional[bool] = None
-    roles: list[str] = []
+    email: EmailStr | None = None
+    full_name: str | None = None
 
 
-class UserInDB(User):
+class User(UserBase):
+    disabled: bool | None = None
+    roles: list[str] = Field(default_factory=list)
+
+
+class UserInDB(UserBase):
     hashed_password: str
 
 
 class UserCreate(BaseModel):
-    username: str
+    username: str = Field(min_length=3, max_length=20, pattern="^[a-zA-Z0-9_]+$")
     email: EmailStr
-    full_name: str
-    password: str
+    full_name: str | None = Field(default=None, max_length=60)
+    password: str = Field(min_length=6, max_length=50)
