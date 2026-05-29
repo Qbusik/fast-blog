2cbbc067a442f746003d2032c7bdc54407f1e42a
Jakub Paluch <qbusik@gmail.com>
2026-05-29 10:21:54 +0200

    Enable SQLAlchemy echo logging

--- db/database.py
+++ db/database.py
@@ -15,7 +15,9 @@ from sqlalchemy.sql import func
 # Database URL - SQLite for development
 DATABASE_URL = "sqlite:///./auth.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}, echo=True
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
